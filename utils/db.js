a24c1f24249923fe3c76b61ab933adf026937a29
jxkimathi <judescriva8@gmail.com>
2024-09-03 12:50:26 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+import mongodb from 'mongodb'
+import Collection from 'mongodb/lib/collection'
+
+class DBClient {
+  constructor () {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { userUnifiedTopology: True });
+    this.client.connect();
+  }
+
+  // Checks if connection is active
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  // Retrieves number of users
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  // Retrieves number of files in database
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  // retrieves a reference to the users
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  // Retrieves a reference to the files
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
